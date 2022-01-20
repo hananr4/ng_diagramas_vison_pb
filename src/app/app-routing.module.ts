222155c5f58496775eaeca0280defaee75b61eab
Hanan Rodriguez <hanan.rodriguez@greensoft.com.ec>
2022-01-20 12:11:53 -0500

    Crear componente ver-diagrama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VerDiagramaComponent } from './ver-diagrama/ver-diagrama.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ver-diagrama',
+    component: VerDiagramaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { DevextremeModule } from './utils/devextreme.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VerDiagramaComponent } from './ver-diagrama/ver-diagrama.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    VerDiagramaComponent,
   ],
   imports: [
     BrowserModule,
